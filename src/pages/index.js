f56a33b05904187e69907fc2ba5603b4485ce9eb
Mark Yabut <markyabut@Marks-MacBook-Pro.local>
2019-04-18 19:59:16 -0400

    Added Products, Contacts

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import BackgroundSection from "../components/Globals/BackgroundSection";
 import Info from "../components/Home/Info";
+import Menu from "../components/Home/Menu";
+import Products from "../components/Home/Products";
+import Contact from "../components/Home/Contact";
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -12,9 +15,11 @@ const IndexPage = ({ data }) => (
     <BackgroundSection
       img={data.img.childImageSharp.fluid}
       title="Abad Stamps"
-      // styleClass="default-background"
     />
     <Info />
+    <Menu items={data.menu} />
+    <Products />
+    <Contact />
   </Layout>
 );
 
@@ -24,6 +29,24 @@ export const query = graphql`
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulStampSet {
+      edges {
+        node {
+          id
+          category
+          title
+          description {
+            description
+          }
+          price
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
