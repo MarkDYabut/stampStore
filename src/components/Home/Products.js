c1a875914289504b2a49306c3166cfb9cf9d53dd
Mark Yabut <markyabut@Marks-MacBook-Pro.local>
2019-05-05 22:51:35 -0400

    fix

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -14,7 +14,7 @@ const getProducts = graphql`
           image {
             fluid(maxHeight: 1100) {
               src
-              ...GatsbyContentfulFluid_tracedSVG
+              ...GatsbyContentfulFluid
             }
           }
         }
